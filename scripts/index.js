0841bf6eeebca6e8c4170f8181244d9d0ecd47db
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-10-23 10:29:55 -0400

    adds first functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,18 @@
-console.log('ass');
+// get photos by search ==>
+// extract IDs ==>
+// display to screen in scrollable gallery // 20 photos per search
+// get location and meta data ==>
+// when image is clicked ==> place point on map and display weather ==>
+// show location info?
 
-console.log('hey what is gucci?');
+// ======================================================================
+// retrieves images, calls functions to manipulate data and draw to screen
+// ======================================================================
+
+function getPhotos(userSearch) {
+    fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickKey}&text=${userSearch}&sort=interestingness-desc&privacy_filter=1&accuracy=16+&has_geo=1&format=json&nojsoncallback=1`)
+        .then(r => r.json())
+        .then(j => j.photos)
+        .then(getPhotoStats)
+        .then(console.log);
+}
